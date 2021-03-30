67c09bfb1f69f7cdeb9db5d1cc38c4b84bd70f55
buugman195 <bugman195@gmail.com>
2021-03-30 22:42:37 +0800

    Add Admin related pages

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="posts-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://media.mehrnews.com/d/2018/10/28/4/2940617.jpg"
       title="Hello there!"
       previewText="This is my first post!"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://media.mehrnews.com/d/2018/10/28/4/2940617.jpg"
       title="Hello there! 2nd"
       previewText="This is my 2nd post!"
     />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://media.mehrnews.com/d/2018/10/28/4/2940617.jpg"
       title="Hello there! 3rd"
       previewText="This is my 3rd post!"
@@ -26,6 +29,12 @@ import PostPreview from '@/components/Posts/PostPreview';
 export default {
   components: {
     PostPreview,
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      require: false,
+    },
   },
 };
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm';
+
+export default {
+  name: 'AdminPostId',
+  layout: 'admin',
+  components: {
+    AdminPostForm,
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Dave',
+        title: 'Good Book',
+        content: 'Super amazing book!',
+        thumbnailLink: 'https://media.mehrnews.com/d/2018/10/28/4/2940617.jpg',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
